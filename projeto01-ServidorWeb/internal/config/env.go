e2c41445169c572c788534db13ffac4321b979a7
Anderson da Silva <adersoosilvaa@gmail.com>
2023-10-18 08:55:02 -0300

    :sparkles: Lendo as variaveis de ambiente

---
+++ projeto01-ServidorWeb/internal/config/env.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Env struct {
+	Host         string
+	Port         string
+	User         string
+	Password     string
+	NameDatabase string
+}
+
+func LoadEnv() Env {
+	godotenv.Load()
+	return Env{
+		Host:         os.Getenv("api-db-host"),
+		Port:         os.Getenv("api-db-port"),
+		User:         os.Getenv("api-db-user"),
+		Password:     os.Getenv("api-db-password"),
+		NameDatabase: os.Getenv("api-db-name"),
+	}
+}
