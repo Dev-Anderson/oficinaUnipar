3c28c99408140293f05c95ac6c4cb9b9e9371dee
citeb.anderson <anderson@citeb.com.br>
2023-10-20 10:25:42 -0300

    :sparkles: Rota de bem vindo para teste da API

--- api-temperatura/pkg/routes/routes.go
+++ api-temperatura/pkg/routes/routes.go
@@ -1,13 +1,17 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"api-temperatura/internal/controllers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
 	main := router.Group("api/v1")
 	{
 		home := main.Group("home")
 		{
-			home.GET("/")
+			home.GET("/", controllers.Home)
 		}
 	}
 	return router
